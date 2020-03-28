afdeacd551dd988d730a4045ac077df43226e0f5
B-Saidy <baijaw93@yahoo.com>
2020-03-28 19:30:50 +0800

    updates

--- payments/urls.py
+++ payments/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='stripe'),
     path('charge/', views.charge, name='charge'),
+    path('', views.HomePageView.as_view(), name='stripe'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -8,7 +8,6 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 class HomePageView(TemplateView):
     
     template_name = 'payment/stripe.html'
-    
     def get_context_data(self, **kwargs): 
         context = super().get_context_data(**kwargs)
         context['key'] = settings.STRIPE_PUBLISHABLE_KEY
