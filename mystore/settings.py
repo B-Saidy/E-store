ac327b32e79b74cb5cdce9b105b7c94b4e800a0b
B-Saidy <baijaw93@yahoo.com>
2020-06-09 14:28:29 +0800

    added allowed host

--- mystore/settings.py
+++ mystore/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%rqxk@$0ue$4ro@=sj(72fvcrl+7w%q&gl%co55c07gx2)l)%b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['unirand.herokuapp.com','localhost']
 
 
 # Application definition
