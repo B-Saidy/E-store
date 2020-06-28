6ce595e7fdefba3577702f2d60295d0492e8c5e7
B-Saidy <baijaw93@yahoo.com>
2020-06-28 23:40:01 +0800

    changes

--- mystore/settings.py
+++ mystore/settings.py
@@ -81,6 +81,7 @@ WSGI_APPLICATION = 'mystore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
